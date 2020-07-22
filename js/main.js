67f2f0ef45439454b7a0f3f4d5b7c69a4553b1cb
Vanda Adams <vandamadams@gmail.com>
2020-07-22 11:22:18 +0100

    added active element js function

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// Scrolls into section when navlink is clicked
+
 const scrollLinks = document.querySelectorAll('.scroll')
 
 scrollLinks.forEach(link => {
@@ -8,4 +10,19 @@ scrollLinks.forEach(link => {
       behavior: 'smooth'
     })
   })
-})
+});
+
+
+// Adds styling to active navlink 
+
+const navLinks =  document.querySelectorAll('a')
+
+navLinks.forEach(link => {
+  link.addEventListener('click', (event) => {
+    const items = document.querySelectorAll('.nav-item')
+    items.forEach(e => {
+      e.classList.remove('active')
+    })
+    link.parentElement.classList.add('active')
+  })
+});
