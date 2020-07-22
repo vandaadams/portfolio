d3486b41eea4ea20681f75f7e8ccba7d8cb1d9ea
Vanda Adams <vandamadams@gmail.com>
2020-07-22 17:47:27 +0100

    added about content

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+
+
 // Scrolls into section when navlink is clicked
 
 const scrollLinks = document.querySelectorAll('.scroll')
@@ -12,17 +14,39 @@ scrollLinks.forEach(link => {
   })
 });
 
+// active class for navbar links
+// https://github.com/camwiegert/in-view
 
-// Adds styling to active navlink 
-
-const navLinks =  document.querySelectorAll('a')
-
-navLinks.forEach(link => {
-  link.addEventListener('click', (event) => {
-    const items = document.querySelectorAll('.nav-item')
-    items.forEach(e => {
-      e.classList.remove('active')
-    })
-    link.parentElement.classList.add('active')
+inView('.section')
+  // adds active class to navlink when section scrolls in
+  .on('enter', section => {
+    scrollLinks.forEach(link => {
+      if ('#' + section.id === link.getAttribute('href')) {
+        link.parentElement.classList.add('active')
+      }
+    });
   })
-});
+  // removes active class from navlink when section scrolls out
+  .on('exit', section => {
+    scrollLinks.forEach(link => {
+      if ('#' + section.id === link.getAttribute('href')) {
+        link.parentElement.classList.remove('active')
+      }
+    });
+  })
+
+  inView.threshold(0.5);
+
+// Adds styling to active navlink
+
+// const navLinks =  document.querySelectorAll('a')
+//
+// navLinks.forEach(link => {
+//   link.addEventListener('click', (event) => {
+//     const items = document.querySelectorAll('.nav-item')
+//     items.forEach(e => {
+//       e.classList.remove('active')
+//     })
+//     link.parentElement.classList.add('active')
+//   })
+// });
